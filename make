3443a20f69cda68db100b25403207b600660e371
Konstantin Slastnoy <sikroz@ispsystem.com>
2014-10-08 17:55:31 +0900

    initial commit

---
+++ make
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import os
+import remote
+import sys
+try:
+	DEFAULT_HOST = os.environ['DEFAULT_SERVER']
+except KeyError:
+	DEFAULT_HOST = 'd64'
+
+def upload_source(args):
+	server = args[1]
+	dst = remote.destination()
+	remote.ssh(server, 'mkdir -p {}'.format(dst))
+	remote.cp(server, os.getcwd() + '/', dst)
+def remove_source(args):
+	server = args[1]
+	dst = remote.destination()
+	remote.ssh(server, 'rm -rf {}'.format(dst))
+def main():
+	args = sys.argv[1:]
+	server = DEFAULT_HOST
+	if len(args) >= 2:
+		if args[0] == 'source':
+			upload_source(args)
+			return
+		if args[0] == 'nosource':
+			remove_source(args)
+			return
+	remote.initdistdir(server)
+	base = remote.get_base()
+	dst = remote.destination()
+	remote.ssh(server, 'mkdir -p {}'.format(dst))
+	remote.cp(server, os.getcwd() + '/', dst)
+	remote.ssh(server, 'cd {} && gmake {} {}'.format(dst, 'BASE=' + base if len(base) else '', ' '.join(args)))
+
+if __name__ == '__main__':
+	main()
---
+++ remote.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+import subprocess
+import os
+CORE = os.path.join(os.environ['HOME'], "ISP", "core-manager")
+def branch(path):
+	branch = ''
+	proc = subprocess.Popen(['git', '-C', path, 'branch'], stdout=subprocess.PIPE)
+	while True:
+		line = proc.stdout.readline()
+		if len(line) and line[0] == '*':
+			branch = line.split(' ')[1].strip()
+			break
+	return branch.replace('/', '-')
+
+def ssh(host, command):
+	print 'ssh -A root@%s "%s"' % (host, command)
+	subprocess.call('ssh -A root@%s "%s"' % (host, command), shell=True)
+def sshout(host, command):
+	print 'ssh -A root@%s "%s"' % (host, command)
+	try:
+		return subprocess.check_output('ssh -A root@%s "%s"' % (host, command), shell=True)
+	except subprocess.CalledProcessError:
+		return ''
+def cp(host, src, dst):
+	print src, dst
+	subprocess.call('rsync -cvr --exclude .build --exclude .git %s root@%s:%s' % (src, host, dst), shell=True)
+	#subprocess.call('rsync -ctvr --delete --exclude .build --exclude .git %s root@%s:%s' % (src, host, dst), shell=True)
+def initdistdir(server):
+	dirname = os.path.basename(os.path.normpath(os.getcwd()))
+	if dirname == 'core-manager':
+		return
+	#Возможно нужно удалять дист постоянно
+	dst = destination(CORE)
+	cp(server, CORE + '/', dst)
+	ssh(server, 'cd {} && gmake -j5 dist DISTDIR={}'.format(dst, get_base()))
+def get_base():
+	#Возможно стоит использовать и папку панели с бранчем
+	dirname = os.path.basename(os.path.normpath(os.getcwd()))
+	if dirname == 'core-manager':
+		return ''
+	return destination(CORE) + "-dist"
+def destination(path=os.getcwd()):
+	dirname = os.path.basename(os.path.normpath(path))
+	return os.path.join("/root", dirname + "-" + branch(path))
